d6baa209cc628bd276cc6b10fa17d8b86beb45a1
= <dreadlord12318@gmail.com>
2021-08-09 14:49:17 +0800

    Updated navigation

---
+++ resources/views/header.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+  <link rel = "stylesheet" href="{{ asset('css/app.css')}}">
+    </head>
+    <body>
+
+<nav>
+     <!-- navbar in here -->
+    <div class = "max-w-10xl mx-auto border border-red-400">
+        <div class = "flex justify-between">
+            <div class = "flex">
+                <!-- Logo -->
+                <div>
+                    <a href = "#">
+                    <img class = "h-6 w-6" src = "/images/LAMB-ID-Icon-Green.png">
+                    </a>
+                </div>
+
+                <!-- Primary Nav -->
+                <div>Primary</div>
+            </div>
+                <!-- Secondary Nav -->
+                <div>Secondary</div>
+        </div>
+    </div>
+
+<!-- Mobile Nav -->
+</nav>
