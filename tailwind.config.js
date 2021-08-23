e69da10a7e68694893fbf122c0acc944408358a1
= <dreadlord12318@gmail.com>
2021-08-23 10:47:33 +0800

    Tailwind with auth preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: [
     './resources/views/**/*.blade.php',
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {
-      colors: {
-        fuchsia: colors.fuchsia,
-      },
-    }
+    extend: {}
   },
   variants: {},
   plugins: [
